471282a669c0bd5ccb7ce269ef2732847ae44533
AnuarSv <sultanbekov1706@gmail.com>
2024-11-04 22:35:30 +0500

    creating models

--- models.py
+++ models.py
@@ -1,21 +1,6 @@
 from sqlalchemy import BigInteger, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
-# singleton
-class Database:
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance.engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
-            cls._instance.async_session = async_sessionmaker(cls._instance.engine)
-        return cls._instance
-
-    def get_session(self):
-        return self._instance.async_session
-
-db = Database()  # Singleton instance
+from sqlalchemy.ext.asyncio import AsyncAttrs
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -25,7 +10,3 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id = mapped_column(BigInteger)
     history: Mapped[str] = mapped_column(Text, nullable=True)
-
-async def async_main():
-    async with db.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)

8d231f0f91496bd06c2cc00291cb1331931532e3
nel1l <nel1l.for.me@gmail.com>
2024-11-04 14:41:46 +0500

    перенесла логику ответа на текстовые сообщения в стратегию, чтобы можно было легко изменять поведение бота

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from commands import StartCommand, HelpCommand, HistoryCommand, DeleteHistoryCommand
+from strategies import HistoryResponseStrategy, GPTResponseStrategy
 
 router = Router()
 
@@ -19,3 +20,11 @@ async def cmd_history(message: Message):
 @router.message(Command("del_history"))
 async def cmd_del_history(message: Message):
     await DeleteHistoryCommand().execute(message)
+
+@router.message(F.text)
+async def cmd_text(message: Message):
+    history = await rq.get_history(message.from_user.id)
+    strategy = GPTResponseStrategy() if "question" in message.text.lower() else HistoryResponseStrategy()
+    response = await strategy.generate_response(message, history)
+    await message.answer(response)
+    await rq.save_history(message.from_user.id, f'{message.text}\n{response}')
